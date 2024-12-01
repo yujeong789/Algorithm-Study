4e11560b581ea3029949bf9bbfeb6ad6ab0fe293
YuJeong <hao_yj@naver.com>
2024-12-01 22:18:15 +0900

    [BOJ] 평범한 배낭 / 골드5 / 40분
    https://www.acmicpc.net/problem/12865

---
+++ 김유정/평범한배낭.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class 평범한배낭 {
+	
+	static int N, K;
+	static int[] W, V;
+	static int[][] dp;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		W = new int[N+1];
+		V = new int[N+1];
+		dp = new int[N+1][K+1];
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= K; j++) {
+				if(W[i]>j) dp[i][j] = dp[i-1][j];
+				else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
+				
+			}
+		}
+		
+		System.out.println(dp[N][K]);
+	}
+}
