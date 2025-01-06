a16aaebe6f419d50da6d3f6f663189ae0f685411
YuJeong <hao_yj@naver.com>
2025-01-06 23:31:13 +0900

    [BOJ] 리모컨 / 골드5 / 2시간
    https://www.acmicpc.net/problem/1107

--- 김유정/Main_1107.java
+++ 김유정/Main_1107.java
@@ -1,41 +1,47 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M, min;
-    static boolean[] broken;
+    static String target, push;
+    static int N, M, now, min, ans;
+    static boolean[] buttons;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        target = br.readLine();
+        N = Integer.parseInt(target);
         M = Integer.parseInt(br.readLine());
-        broken = new boolean[10];
-
-        if (M > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                int n = Integer.parseInt(st.nextToken());
-                broken[n] = true;
+        buttons = new boolean[10];
+        if(M>0){
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < M; i++){
+                buttons[Integer.parseInt(st.nextToken())] = true;
             }
         }
-        
-        min = Math.abs(N - 100); // +/-로만 이동할 경우
-        if (M < 10) { // 누를 수 있는 버튼이 있을 때만 탐색
-            findMin(0, 0);
+//        System.out.println(Arrays.toString(buttons));
+        min = Math.abs(100-N);
+        for(int i = 0; i < 999_999; i++) {
+        	int len = possible(i);
+        	if(len>=0) {
+        		min = Math.min(min, len+Math.abs(N-i));
+        	}
+        	
         }
         System.out.println(min);
     }
 
-    private static void findMin(int num, int cnt) {
-        if (cnt > 6) return;
+	private static int possible(int i) {
+		int cnt = 0;
+		if(i==0 && !buttons[i]) return 1;
+		while(i>0) {
+			int value = i%10;
+			if(buttons[value]) return -1;
+			cnt++;
+			i /= 10;
+		}
+		return cnt;
+	}
 
-        int press = Math.abs(N - num);
-        min = Math.min(min, press + cnt);
-
-        for (int i = 0; i < 10; i++) {
-            if (!broken[i]) {
-                findMin(num * 10 + i, cnt + 1);
-            }
-        }
-    }
 }
